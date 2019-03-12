068d3df8e76f2530bca944a2f27a46ead09d7481
vinfry <mercurial.sp@gmail.com>
2019-03-12 15:14:03 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,91 @@
+var ftp = require('basic-ftp');
+var fs = require('fs');
+
+function upload(action, settings) {
+    let host = action.params.HOST;
+    let user = action.params.USER;
+    let password = settings.PASSWORD;
+    let secure = "true";
+    let port = action.params.PORT;
+    let localPath = action.params.LOCALFOLDER;
+    let remotePath = action.params.REMOTEFOLDER;
+    var client = new ftp.Client();
+    client.ftp.verbose = true;
+    return client.access({host: host, user: user, password: password, port: port, secure: secure}).then(function () {
+        let stats = fs.statSync(localPath);
+        if (stats.isDirectory()) {
+            return client.ensureDir(remotePath).then(function () {
+                return client.uploadDir(localPath).then(function () {
+                    client.close()
+                })
+        })
+        } else {
+            return client.upload(fs.createReadStream(localPath), remotePath).then(function () {
+                client.close()
+            })
+        }
+    })
+}
+
+
+function remove(action, settings) {
+    let host = action.params.HOST;
+    let user = action.params.USER;
+    let password = settings.PASSWORD;
+    let secure = "true";
+    let port = action.params.PORT;
+    let localPath = action.params.LOCALFOLDER;
+    let remotePath = action.params.REMOTEFOLDER;
+    var client = new ftp.Client();
+    client.ftp.verbose = true;
+    return client.access({host: host, user: user, password: password, port: port, secure: secure}).then(function () {
+        let stats = fs.statSync(localPath);
+        if (stats.isDirectory()) {
+            return client.removeDir(localPath, remotePath).then(function () {
+                client.close()
+            })
+        } else {
+            return client.remove(remotePath).then(function () {
+                client.close()
+            })
+        }
+    })
+}
+
+function removeDir(action, settings) {
+    let host = action.params.HOST;
+    let user = action.params.USER;
+    let password = settings.PASSWORD;
+    let secure = "true";
+    let port = action.params.PORT;
+    let remotePath = action.params.REMOTEFOLDER;
+    var client = new ftp.Client();
+    client.ftp.verbose = true;
+    return client.access({host: host, user: user, password: password, port: port, secure: secure}).then(function () {
+        return client.removeDir(remotePath).then(function () {
+                client.close()
+            })
+    })
+}
+
+function remove(action, settings) {
+    let host = action.params.HOST;
+    let user = action.params.USER;
+    let password = settings.PASSWORD;
+    let secure = "true";
+    let port = action.params.PORT;
+    let remotePath = action.params.REMOTEFILE;
+    var client = new ftp.Client();
+    client.ftp.verbose = true;
+    return client.access({host: host, user: user, password: password, port: port, secure: secure}).then(function () {
+        return client.remove(remotePath).then(function () {
+                client.close()
+        })
+    })
+}
+
+module.exports = {
+    upload: upload,
+    remove: remove,
+    removeDir: removeDir
+}
