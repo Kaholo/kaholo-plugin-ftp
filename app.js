7388aeb9fc5e517a22067d55a9099bfdae3a11a1
xkcm <xkcm16@gmail.com>
2022-05-13 13:06:30 +0200

    Add template files

--- app.js
+++ app.js
@@ -3,57 +3,60 @@ const fs = require('fs-extra')
 const path = require('path');
 
 async function upload(action, settings) {
-    const options = getOptions(action, settings);
-    const localPath = action.params.localPath;
-    const remotePath = action.params.remotePath;
-    const stats = fs.statSync(localPath);
-    const remoteDir = stats.isDirectory() ? remotePath : path.dirname(remotePath);
-    const ftpFunc = async function(client){
-        await client.ensureDir(remoteDir);
-        return stats.isDirectory() ? 
-            client.uploadFromDir(localPath) :
-            client.uploadFrom(localPath, path.basename(remotePath));
-    }
-
-    return runFtpFunction(options, ftpFunc);
+  const options = getOptions(action, settings);
+  const localPath = action.params.localPath;
+  const remotePath = action.params.remotePath;
+  const stats = fs.statSync(localPath);
+  const remoteDir = stats.isDirectory() ? remotePath : path.dirname(remotePath);
+  const ftpFunc = async function(client){
+    await client.ensureDir(remoteDir);
+    return stats.isDirectory() ? 
+    client.uploadFromDir(localPath) :
+    client.uploadFrom(localPath, path.basename(remotePath));
+  }
+  
+  return runFtpFunction(options, ftpFunc);
 }
 
 async function remove(action, settings) {
-    const options = getOptions(action, settings);
-    const {remotePath, objType} = action.params;
-    
-    const ftpFunc = (objType == "Folder") ? client => client.removeDir(remotePath) : 
-        client => client.remove(remotePath);
-    return runFtpFunction(options, ftpFunc);
+  const options = getOptions(action, settings);
+  const {remotePath, objType} = action.params;
+  
+  const ftpFunc = (
+    (objType == "Folder")
+      ? client => client.removeDir(remotePath)
+      : client => client.remove(remotePath)
+  );
+  return runFtpFunction(options, ftpFunc);
 }
 
 async function downloadFile(action, settings) {
-    const options = getOptions(action, settings);
-    const localPath = action.params.localPath;
-    const remotePath = action.params.remotePath;
-    fs.ensureDirSync(path.dirname(localPath));
-    return runFtpFunction(options, client => client.downloadTo(localPath, remotePath));
+  const options = getOptions(action, settings);
+  const localPath = action.params.localPath;
+  const remotePath = action.params.remotePath;
+  fs.ensureDirSync(path.dirname(localPath));
+  return runFtpFunction(options, client => client.downloadTo(localPath, remotePath));
 }
 
 async function downloadDir(action, settings) {
-    const options = getOptions(action, settings);
-    const localPath = action.params.localPath;
-    const remotePath = action.params.remotePath;
-    fs.ensureDirSync(localPath);
-    return runFtpFunction(options, client => client.downloadToDir(localPath, remotePath));
+  const options = getOptions(action, settings);
+  const localPath = action.params.localPath;
+  const remotePath = action.params.remotePath;
+  fs.ensureDirSync(localPath);
+  return runFtpFunction(options, client => client.downloadToDir(localPath, remotePath));
 }
 
 async function list(action, settings) {
-    const options = getOptions(action, settings);
-    const remotePath = action.params.remotePath;
-
-    return runFtpFunction(options, client => client.list(remotePath));
+  const options = getOptions(action, settings);
+  const remotePath = action.params.remotePath;
+  
+  return runFtpFunction(options, client => client.list(remotePath));
 }
 
 module.exports = {
-    upload,
-    remove,
-    downloadFile,
-    downloadDir,
-    list
+  upload,
+  remove,
+  downloadFile,
+  downloadDir,
+  list
 }
